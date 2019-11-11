4465fe923cc85c1254355fd0e841253c46072fbf
Angie Roach <angieroach4@gmail.com>
2019-11-10 19:51:28 -0500

    db and packages init

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+-- Drops the burgers_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "burger_db" database --
+CREATE DATABASE burgers_db;
+
+-- Makes it so all of the following code will affect burger_db --
+USE burgers_db;
+
+-- Creates the table "burgers" within burger_db --
+CREATE TABLE burgers (
+    -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows. --
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  -- Makes a string column called "burger_name" which cannot contain null --
+  burger_name VARCHAR(200) NOT NULL,
+  -- Makes a boolean column called "devoured" which cannot contain null --
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Creates new rows containing data in all named columns --
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Blue Cheese and Onion Jam Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("ChirDuckin Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Kobe Burger with Red Wine Carmelized Onions", FALSE);
+
+
+
