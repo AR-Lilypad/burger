09a8ade0e4f8d1768cea50c6ea60344c7ab2f440
Angie Roach <angieroach4@gmail.com>
2019-11-17 17:07:20 -0500

    coded server.js file

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 // Dependencies
 // ===========================================================
 const express = require("express");
+const exphbs = require("express-handlebars");
 
-
-
-
+let app = express();
 
 // code needed to deploy to Heroku
 const PORT = process.env.PORT || 8080;
-
-let app = express();
 
 //serve static content for the app from the "public" folder - like assets: css, images 
 app.use(express.static("public"));
@@ -17,3 +14,17 @@ app.use(express.static("public"));
 //parse the application body
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// route dependency
+let routes = require("./controllers/burgers_controller.js");
+
+// port listening code
+app.listen(PORT, function() {
+    console.log("server listening on: http://localhost:" + PORT);
+})
+
+
