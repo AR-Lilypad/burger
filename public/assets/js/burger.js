512d52225fdaa5e23c409d4833eb7fa40ef68f05
Angie Roach <angieroach4@gmail.com>
2020-01-22 01:19:37 -0500

    checking inputs and ids

---
+++ public/assets/js/burger.js
@@ -0,0 +1,38 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".devour").on("click", function(event) {
+    let id = $(this).data("id");
+
+    let eaten = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eaten
+    }).then(function() {
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    let newBurger = {
+      name: $("#burger")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("Inserted new Burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
